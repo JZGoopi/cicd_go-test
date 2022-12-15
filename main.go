f394d611b114182193d422281021315b825b3d2f
JZ <jyunzn@gmail.com>
2022-12-15 23:11:15 +0800

    故意出錯 (#1)
    * 故意出錯
    
    * fix it
    
    * 寫點注釋
    
    * 添加密碼
    
    * 去抓環境變量 password 來賦值

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"apple/calc"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println(calc.Add(3,5))
+	var password = os.Getenv("password")
+	fmt.Println(calc.Add(3,5), password)
 }
